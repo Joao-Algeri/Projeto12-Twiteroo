7b03cb67950ea5f44244571ebf3b469dfc38c564
JoaoAlgeri <joaoalgeri@gmail.com>
2023-04-10 09:25:26 -0300

    Post sign-up

--- src/app.js
+++ src/app.js
@@ -4,8 +4,15 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
+ 
+const users=[];
+const tweets=[];
 
-
+app.post("/sign-up",(req,res)=>{
+    const user=req.body;
+    users.push(user);
+    res.send("OK")
+})
 const PORT=5000
 
 app.listen(PORT, () =>console.log(`servidor rodando em => ${PORT}`))
